1f1a067f1acc9b28271675a1690aef08a11cd8ff
Tera <24725862+teraa@users.noreply.github.com>
2022-12-02 07:31:16 +0100

    day2

--- Day02.a/Program.cs
+++ Day02.a/Program.cs
@@ -3,7 +3,7 @@ foreach (var line in GetInput())
 {
     int i = line[0] - 'A';
     int j = line[2] - 'X';
-    score += (2 - (i - j + 4) % 3) * 3 + j + 1;
+    score += (j + 1) + (2 - (i - j + 4) % 3) * 3;
 }
 
 WriteLine(score);
--- Day02.b/Program.cs
+++ Day02.b/Program.cs
@@ -3,7 +3,7 @@ foreach (var line in GetInput())
 {
     int i = line[0] - 'A';
     int j = line[2] - 'X';
-    score += j * 3 + (i + 2 + j) % 3 + 1;
+    score += (j * 3) + (i + j + 2) % 3 + 1;
 }
 
 WriteLine(score);
