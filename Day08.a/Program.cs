7928161c0d55fa36f9812616399c1f3f8e6d46b0
Tera <24725862+teraa@users.noreply.github.com>
2022-12-08 07:28:08 +0100

    day8

---
+++ Day08.a/Program.cs
@@ -0,0 +1,20 @@
+var input = GetInput().ToList();
+int result = 0;
+
+for (int y = 0; y < input.Count; y++)
+{
+    for (int x = 0; x < input[y].Length; x++)
+    {
+        char c = input[y][x];
+        string row = input[y];
+        string col = new string(input.Select(line => line[x]).ToArray());
+
+        if (row[..x].All(e => e < c) ||
+            row[(x + 1)..].All(e => e < c) ||
+            col[..y].All(e => e < c) ||
+            col[(y + 1)..].All(e => e < c))
+            result++;
+    }
+}
+
+WriteLine(result);
---
+++ Day08.b/Program.cs
@@ -0,0 +1,31 @@
+var input = GetInput().ToList();
+int result = 0;
+
+int maxj = input.Count - 1;
+for (int j = 0; j <= maxj; j++)
+{
+    int maxi = input[j].Length - 1;
+    for (int i = 0; i <= maxi; i++)
+    {
+        char c = input[j][i];
+        string row = input[j];
+        string col = new string(input.Select(line => line[i]).ToArray());
+
+        int im = row[..i].Reverse().TakeWhile(x => x < c).Count();
+        int ip = row[(i + 1)..].TakeWhile(x => x < c).Count();
+        int jm = col[..j].Reverse().TakeWhile(x => x < c).Count();
+        int jp = col[(j + 1)..].TakeWhile(x => x < c).Count();
+
+        if (i - im > 1) im++;
+        if (j - jm > 1) jm++;
+        if (i + ip < maxi) ip++;
+        if (j + jp < maxj) jp++;
+
+        int score = im * ip * jm * jp;
+
+        if (score > result)
+            result = score;
+    }
+}
+
+WriteLine(result);
